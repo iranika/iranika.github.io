54f2a15e8434439bfafdb8941b77bd14ca3bd095
iranika <7370486+iranika@users.noreply.github.com>
2023-07-22 07:23:55 +0900

    add: BrowseNowRIP for Misskey.ioの追加

---
+++ bookmarklet/js/BrowseNow4Misskey.js
@@ -0,0 +1,1 @@
+javascript: (function (e) { var href = `https://misskey.io/share?text=${e(document.title)}(${e(location.href)})`; window.open(href, null, 'width=520,height=500'); })(encodeURIComponent);
